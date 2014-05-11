a81cd7816200f625ef71df1ac0be7efac17b3927
Joel Berger <joel.a.berger@gmail.com>
2014-05-11 06:23:42 -0500

    Improve the bot to have a more flexible set of tasks, including dependencies

--- ex/cpan.pl
+++ ex/cpan.pl
@@ -4,29 +4,56 @@ use Mojo::Base -strict;
 
 use Mojo::FriendFeed;
 use Mojo::IOLoop;
+use Mojo::UserAgent;
 use Mojo::DOM;
 use Mojo::URL;
 use Mojo::IRC;
+use List::Util 'first';
+
+use DDP;
 
 use Getopt::Long;
 
-GetOptions(
-  'channel=s'  => \(my $chan   = 'release'),
-  'nickname=s' => \(my $nick   = 'release_bot'),
-  'pattern=s'  => \my $pattern,
-  'server=s'   => \(my $server = 'irc.perl.org:6667'),
-  'user=s'     => \(my $user   = 'new cpan releases'), 
+my $conf_file = shift or die "A configuration file is required.\n";
+
+my %defaults = (
+  nickname => 'release_bot',
+  server   => 'irc.perl.org:6667',
+  user     => 'new cpan releases',
+  jobs     => [],
 );
 
-$chan = "#$chan" unless $chan =~ /^#/;
-$pattern = qr/$pattern/ if $pattern;
+my %conf = (%defaults, %{ do $conf_file });
 
+my $ua = Mojo::UserAgent->new;
+
+my $join = sub { shift->write( join => shift ) };
 my $send = sub { shift->write( privmsg => shift, ":@_" ) };
 
+sub parse {
+  my $body = shift;
+  my ($dist, $version) = $body =~ /^(\S+) (\S+)/;
+  my $dom = Mojo::DOM->new($body);
+  my $file_url = Mojo::URL->new($dom->at('a')->{href});
+  my $pause_id = $file_url->path->parts->[-2];
+
+  my $deps = $ua->get("http://api.metacpan.org/v0/release/$dist")->res->json('/dependency') || [];
+  my @deps = map { $_->{module} } @$deps; # } # highlight fix
+
+  return {
+    dist     => $dist,
+    version  => $version,
+    file_url => $file_url,
+    pause_id => $pause_id,
+    text     => $dom->text,
+    deps     => \@deps,
+  };
+}
+
 my $irc = Mojo::IRC->new(
-  nick   => $nick,
-  user   => $user,
-  server => $server,
+  nick   => $conf{nickname},
+  user   => $conf{user},
+  server => $conf{server},
 );
 $irc->register_default_event_handlers;
 $irc->connect(sub{
@@ -35,25 +62,33 @@ $irc->connect(sub{
     warn $err;
     exit 1;
   }
-  $irc->write( join => $chan );
+  foreach my $job (@{ $conf{jobs} }) {
+    next unless my $chan = $job->{channel};
+    $irc->$join($chan) if $chan =~ /^#/;
+  }
 });
 
 my $ff = Mojo::FriendFeed->new( request => '/feed/cpan' );
 
 $ff->on( entry => sub {
   my ($self, $entry) = @_;
-  my $dom = Mojo::DOM->new($entry->{body});
-  my $file_url = Mojo::URL->new($dom->at('a')->{href});
-  my ($pauseid, $file) = @{$file_url->path->parts}[-2,-1];
-  $file =~ s/\.tar\.gz//;
+  my $data = parse($entry->{body});
   
-  my $msg = $dom->text . " http://metacpan.org/release/$pauseid/$file";
-  if ($pattern and $msg !~ $pattern) {
-    return;
-  }
-
-  $irc->$send( $chan => $msg );
+  my $msg = $data->{text} . " http://metacpan.org/release/$data->{pause_id}/$data->{dist}-$data->{version}";
   say $msg;
+  p $data;
+
+  my @deps = @{ $data->{deps} || [] };
+
+  for my $job (@{ $conf{jobs} }) {
+    if (my $filter = $job->{dist}) {
+      next unless $data->{dist} =~ $filter;
+    }
+    if (my $filter = $job->{deps}) {
+      next unless first { $_ =~ $filter } @deps;
+    }
+    $irc->$send( $job->{channel} => $msg );
+  }
 });
 
 $ff->on( error => sub { warn "$_[1]\n"; shift->listen } );
